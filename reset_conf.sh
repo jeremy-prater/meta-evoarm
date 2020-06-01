bc03a40c43778a76c125ec5246808e406f88bd62
Jeremy Prater <silverevo03@gmail.com>
2020-06-01 13:06:13 -0700

    - Extract to meta-layer for evoarm

--- reset_conf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ROOTDIR=$(realpath `git rev-parse --show-cdup`)
-mkdir -p $ROOTDIR/build/conf
-
-cp -v $ROOTDIR/meta-evoarm/conf/local.conf $ROOTDIR/build/conf/local.conf
-cp -v $ROOTDIR/meta-evoarm/conf/bblayers.conf $ROOTDIR/build/conf/bblayers.conf
